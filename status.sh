0788158401544e8432f5b0815708c8f2e615215b
Arnav Jain <35961071+myNameArnav@users.noreply.github.com>
2021-04-13 00:41:41 +0530

    QoL improvments
    Reduced LoC and cleaned the code a bit

--- status.sh
+++ status.sh
@@ -1,94 +1,53 @@
-#!/bin/sh
+#!/bin/zsh
 echo ""
 
-arg="${1:-status}"
+arg=$1
 
-if [ $arg = "status" ]
-then
-	echo -n ">> Plex:	"
-	sudo systemctl $arg plexmediaserver.service | grep -F active
-	echo -n ">> Sonarr:	"
-	sudo systemctl $arg sonarr.service | grep -F active
-	echo -n ">> Radarr:	"
-	sudo systemctl $arg radarr.service | grep -F active
-	echo -n ">> Jackett:	"
-	sudo systemctl $arg jackett.service | grep -F active
-	echo -n ">> PowerTunnel:	"
-	sudo systemctl $arg pt.service | grep -F active
-	echo -n ">> PiHole:	"
-	sudo systemctl $arg pihole-FTL.service | grep -F active
-	echo -n ">> Unbound:	"
-	sudo systemctl $arg unbound.service | grep -F active
-	echo ""
-
-
-elif [ $arg = "stop" ] 
+if [ "$arg" = "stop" ] 
 then	
-	echo -n ">> Plex:	"
+	
 	sudo systemctl $arg plexmediaserver.service
-	systemctl status plexmediaserver.service | grep -F active
-	echo -n ">> Sonarr:	"
 	sudo systemctl $arg sonarr.service
-	systemctl status sonarr.service | grep -F active
-	echo -n ">> Radarr:	"
 	sudo systemctl $arg radarr.service
-	systemctl status radarr.service | grep -F active
-	echo -n ">> Jackett:	"
 	sudo systemctl $arg jackett.service
-	systemctl status jackett.service | grep -F active
-	echo -n ">> PowerTunnel:	"
 	sudo systemctl $arg pt.service
-	systemctl status pt.service | grep -F active
-	echo -n ">> PiHole:	"
 	sudo systemctl $arg pihole-FTL.service
-	systemctl status pihole-FTL.service | grep -F active
-	echo ""
+	sudo systemctl $arg unbound.service
 
-elif [ $arg = "start" ] 
+elif [ "$arg" = "start" ] 
 then
-	echo -n ">> Plex:	"
 	sudo systemctl $arg plexmediaserver.service
-	systemctl status plexmediaserver.service | grep -F active
-	echo -n ">> Sonarr:	"
 	sudo systemctl $arg sonarr.service
-	systemctl status sonarr.service | grep -F active
-	echo -n ">> Radarr:	"
 	sudo systemctl $arg radarr.service
-	systemctl status radarr.service | grep -F active
-	echo -n ">> Jackett:	"
 	sudo systemctl $arg jackett.service
-	systemctl status jackett.service | grep -F active
-	echo -n ">> PowerTunnel:	"
 	sudo systemctl $arg pt.service
-	systemctl status pt.service | grep -F active
-	echo -n ">> PiHole:	"
 	sudo systemctl $arg pihole-FTL.service
-	systemctl status pihole-FTL.service | grep -F active
-	echo ""
+	sudo systemctl $arg unbound.service
 
-elif [ $arg = "restart" ] 
+elif [ "$arg" = "restart" ] 
 then
-	echo -n ">> Plex:	"
 	sudo systemctl $arg plexmediaserver.service
-	systemctl status plexmediaserver.service | grep -F active 
-	echo -n ">> Sonarr:	"
 	sudo systemctl $arg sonarr.service
-	systemctl status sonarr.service | grep -F active
-	echo -n ">> Radarr:	"
 	sudo systemctl $arg radarr.service
-	systemctl status radarr.service | grep -F active
-	echo -n ">> Jackett:	"
 	sudo systemctl $arg jackett.service
-	systemctl status jackett.service | grep -F active
-	echo -n ">> PowerTunnel:	"
 	sudo systemctl $arg pt.service
-	systemctl status pt.service | grep -F active
-	echo -n ">> PiHole:	"
 	sudo systemctl $arg pihole-FTL.service
-	systemctl status pihole-FTL.service | grep -F active
-	echo ""
-
-else
-	echo "Wrong input !"
+	sudo systemctl $arg unbound.service
 fi
+
+echo -n ">> Plex:	"
+systemctl status plexmediaserver.service | grep -F active
+echo -n ">> Sonarr:	"
+systemctl status sonarr.service | grep -F active
+echo -n ">> Radarr:	"
+systemctl status radarr.service | grep -F active
+echo -n ">> Jackett:	"
+systemctl status jackett.service | grep -F active
+echo -n ">> PowerTunnel:	"
+systemctl status pt.service | grep -F active
+echo -n ">> PiHole:	"
+systemctl status pihole-FTL.service | grep -F active
+echo -n ">> Unbound:	"
+systemctl status unbound.service | grep -F active
+echo ""
 
